e87beea513215ff865625d3f3a3b2ded31157d21
MohammadAmin Raeisi <79477962+mhmdaminraeisi@users.noreply.github.com>
2023-05-06 03:37:45 +0330

    Add springboot and postgres dependencies and configurations.

---
+++ src/main/java/ir/teias/Configuration.java
@@ -0,0 +1,27 @@
+package ir.teias;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Configuration {
+    public static final String sqlUser;
+    public static final String sqlPassword;
+    public static final String sqlUrl;
+    static {
+        try {
+            String filePath = new File("").getAbsolutePath();
+            filePath += "/src/main/resources/application.properties";
+            FileInputStream inputStream = new FileInputStream(filePath);
+            Properties config = new Properties();
+            config.load(inputStream);
+            sqlUser = config.getProperty("spring.datasource.username");
+            sqlPassword = config.getProperty("spring.datasource.password");
+            sqlUrl = config.getProperty("spring.datasource.url");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
